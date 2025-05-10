31e33a81d34d5a37d9ceb3af88a9a9179455a7b1
Jon Pekkarinen <77648523+jonpekk@users.noreply.github.com>
2025-05-09 22:44:17 -0400

    basic global linter config

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -28,13 +28,14 @@ export const register = async (req: Request, res: Response) => {
   }
 };
 
-export const login = async (req: Request, res: Response): Promise<any> => {
+export const login = async (req: Request, res: Response): Promise<void> => {
   const { email, password } = req.body;
   console.log(email, password)
 
   const user = await prisma.user.findUnique({ where: { email } });
   if (!user || !(await bcrypt.compare(password, user.password))) {
-    return res.status(401).json({ error: 'Invalid credentials' });
+    res.status(401).json({ error: 'Invalid credentials' });
+    return;
   }
 
   const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET as string, {
