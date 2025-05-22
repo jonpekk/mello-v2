cc8555f97d51b9dabcebc1aeb029fb8463367880
Jon Pekkarinen <77648523+jonpekk@users.noreply.github.com>
2025-05-21 21:10:52 -0400

    basic jest setup with initial test

--- server/server.ts
+++ server/server.ts
@@ -1,31 +1,5 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes';
-import { authenticate } from './middleware/auth';
-import cookieParser from 'cookie-parser';
-import counterRoutes from './routes/counterRoutes';
-
-dotenv.config();
-
-const app = express();
+import { app } from './app'
 const port = process.env.PORT || 5500;
-
-// Middleware
-app.use(express.json());
-app.use(cookieParser());
-
-
-// Example route
-app.get('/', async (req, res) => {
-  res.json({ test: 'sent' });
-});
-
-app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/counter', counterRoutes)
-
-app.get('/api/v1/profile', authenticate, (req, res) => {
-  res.json({ auth: 'BOOM' })
-});
 
 // Start the server
 app.listen(port, () => {
