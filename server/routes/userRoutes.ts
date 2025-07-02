1d475cfe8d3b56ffc75315cf64eb720f50f78b98
Jon Pekkarinen <77648523+jonpekk@users.noreply.github.com>
2025-07-02 12:57:33 -0400

    sanitize form inputs

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { getProfile, editProfile } from '../controllers/userController';
 import { optionalAuth, requireAuth } from '../middleware/auth';
+import { sanitizeBody } from '../middleware/sanitize';
 
 const router = express.Router();
 
 router.get('/:id', optionalAuth, getProfile)
-router.patch('/:id', requireAuth, editProfile)
+router.patch('/:id', requireAuth, sanitizeBody, editProfile)
 
 export default router;
